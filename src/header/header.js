23a5132f7e36272d3313b1aa414a0762d21f6bf4
Michael Kroth <mkroth65@gmail.com>
2018-08-26 21:21:13 -0500

    Header component, tachyons stylesheet

---
+++ src/header/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div id="header" className="w-25 bg-navy ba b--green br2">
+      <h1 className="light-green"> My Movies</h1>
+    </div>
+  );
+};
+
+export default Header;
